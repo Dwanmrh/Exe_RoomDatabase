bcc3638fce5f6650b6f672e7fb947faa8bd5497f
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-16 15:01:05 +0700

    Membuat Fungsi untuk mengakses data Mahasiswa

--- app/src/main/java/com/dwan/exe_roomdatabase/data/database/KrsDatabase.kt
+++ app/src/main/java/com/dwan/exe_roomdatabase/data/database/KrsDatabase.kt
@@ -2,8 +2,11 @@ package com.dwan.exe_roomdatabase.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.dwan.exe_roomdatabase.data.dao.MahasiswaDao
 import com.dwan.exe_roomdatabase.data.entity.Mahasiswa
 
 @Database(entities = [Mahasiswa::class], version = 1, exportSchema = false) // Entitas tabel Mahasiswa
 abstract class KrsDatabase: RoomDatabase() {
+
+    abstract fun mahasiswaDao(): MahasiswaDao // Fungsi untuk mengakses data Mahasiswa
 }
