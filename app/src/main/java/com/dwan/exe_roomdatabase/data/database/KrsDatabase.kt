4729106f2f7e683b0e949d43a74a21016fbd54f8
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-16 14:59:21 +0700

    Membuat Database dengan entitas tabel Mahasiswa

--- app/src/main/java/com/dwan/exe_roomdatabase/data/database/KrsDatabase.kt
+++ app/src/main/java/com/dwan/exe_roomdatabase/data/database/KrsDatabase.kt
@@ -1,4 +1,9 @@
 package com.dwan.exe_roomdatabase.data.database
 
-class KrsDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.dwan.exe_roomdatabase.data.entity.Mahasiswa
+
+@Database(entities = [Mahasiswa::class], version = 1, exportSchema = false) // Entitas tabel Mahasiswa
+abstract class KrsDatabase: RoomDatabase() {
 }
